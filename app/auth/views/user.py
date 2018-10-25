20fb6847caafa7d06ab46a0a2bdfa33b86d1c7cf
acodercat <1067302838@qq.com>
2018-10-25 19:25:11 +0800

    修改蓝图

--- app/auth/views/user.py
+++ app/auth/views/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .. import blueprint
+from .. import auth_blueprint
 from ... import db
 from app.auth.models import User
 
 
-@blueprint.route('/login')
+@auth_blueprint.route('/login')
 def login():
     user = User()
     # user.email = '12121'
